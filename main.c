4ec89fe3674196a29bd8c237267142c73d7f0289
David <david.inabinette@gmail.com>
2025-01-19 20:00:58 -0600

    first commit

---
+++ main.c
@@ -0,0 +1,72 @@
+/*boilerplate includes*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#include <string.h>
+#include <math.h>
+
+
+
+
+//---------------------------------------------
+/*Tasks*/
+//---------------------------------------------
+
+/* Write a program that does the following each time it loops:
+1. Generates 5 random numbers from 1 to 50
+2. Keep track of the largest number and print it out at the end of the loop
+3. Stores those 5 random numbers in an array
+4. Print the array
+5. Create a loop that prints the word “Yes” 5 times
+6. Create a loop that stops after it has run 10 times
+7. Use print statement(s) to show how many times the loop ran
+8. Ask the user if they want to run again
+9. Keep track of how many times the program loops and print it out at the end */
+
+
+
+
+
+
+//---------------------------------------------
+/*global variable creation*/
+//---------------------------------------------
+
+
+
+//---------------------------------------------
+/*Functions*/
+//---------------------------------------------
+
+
+//---------------------------------------------
+/*begin main*/
+//---------------------------------------------
+
+int main()
+{
+
+    // user variables
+    
+
+    //---------------------------------------------
+    /*variable testing*/
+    //---------------------------------------------
+    
+    
+    //---------------------------------------------
+    /*begin functions*/
+    //---------------------------------------------
+
+
+
+
+
+
+
+    //---------------------------------------------
+    /*end program*/
+    //---------------------------------------------
+    return 0;
+}
